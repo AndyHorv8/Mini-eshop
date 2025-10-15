18700bdd536344c1724ae15f91a384f383827987
AndyHorv8 <andreahorvathova8@gmail.com>
2025-10-15 19:19:21 +0200

    Added home page with welcome text and button

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mini-eshop/',
+  base: '/Mini-eshop/', // 👈 MUSÍ mať presne veľké M
 })
